721484016754f313275ab110305e2328d4d379b2
sleepyheadp <17863965581@163.com>
2023-06-13 23:26:24 +0800

    2023-06-13：JSX与HTML的区别

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main className="container">
+			<h1
+				style={{
+					color: "pink",
+					fontSize: "32px",
+				}}
+			>
+				hello React!
+			</h1>
+			<div>
+				<textarea placeholder="写点什么吧..." cols="30" rows="5"></textarea>
+				<button>发布</button>
+			</div>
+		</main>
+	);
 }
 
 export default App;

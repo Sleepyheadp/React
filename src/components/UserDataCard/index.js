35792013a29f817e6e077cf07177d25ad5833d0b
sleepyheadp <17863965581@163.com>
2023-06-16 11:55:45 +0800

    2023-06-16：sync

--- src/components/UserDataCard/index.js
+++ src/components/UserDataCard/index.js
@@ -3,11 +3,12 @@ import './style.css'
 
 // 父组件传递的是userDate，这里为什么要把reset展开，我没定义reset呀？
 // 答：这里的reset是父组件传递的props，包含了userData和message
-function UserDataCard({className,pClass,message,...reset}){
+// 从传递的所有props当中把className，pClass，message，单曲提取出出来，剩下的全部放到了...reset当中.
+function UserDataCard({className,pClass,message,...rest}){
   // console.log('reset',reset)
   return (
     <div className={`userDataCard ${className}` }>
-      <UserData {...reset}/>
+      <UserData {...rest}/>
       <p className={pClass}>{message}</p>
     </div>
   )

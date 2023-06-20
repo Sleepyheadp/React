069f1269b9ab4216256c021b2c42e3500b327726
sleepyheadp <17863965581@163.com>
2023-06-20 10:15:49 +0800

    2023-06-20：React操作DOM（父组件获取子组件实例 案例：控制视频播放

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
+import Sea from "./assets/videos/sea.mp4";
+import { ReactComponent as PlayComp } from "./assets/images/play.svg";
+import { ReactComponent as PauseComp } from "./assets/images/pause.svg";
 function App() {
 	return (
 		<div className="container">
 			<AddOnce />
 			{/* React通过useRef操作DOM实现视频的播放和暂停*/}
-			<VideoPlayer />
+			<VideoPlayerBox/>
 		</div>
 	)
 }
@@ -22,5 +25,39 @@ function AddOnce(){
 	)
 }
 
-
+function VideoPlayerBox(){
+	// 播放状态
+	const [isPlaying, setIsPlaying] = useState(false);
+	// 播放器DOM
+	const videoRef = useRef();
+	//直接在组件中调用没有效果，因为这个时候DOM还没有渲染出来，需要在useEffect中调用，
+	// 也就是在组件渲染完成之后调用
+	// videoRef.current.play()
+	useEffect(()=>{
+		videoRef.current.play()
+		setIsPlaying(true)
+	},[])
+	// 播放
+	function handlePlay() {
+		// 获取DOM
+		const video = videoRef.current;
+		// 播放
+		if (isPlaying) {
+			// 这个pause和play是video自带的方法
+			video.pause();
+		} else {
+			video.play();
+		}
+		// 更新播放状态
+		setIsPlaying(!isPlaying);
+	}
+	return (
+		<>
+			<VideoPlayer src={Sea} ref={videoRef}/>
+			<button onClick={handlePlay}>
+				{isPlaying ? <PauseComp/> : <PlayComp/>}
+			</button>
+		</>
+	)
+}
 export default App;
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,41 +1,10 @@
-import Sea from "../../assets/videos/sea.mp4";
-import { ReactComponent as PlayComp } from "../../assets/images/play.svg";
-import { ReactComponent as PauseComp } from "../../assets/images/pause.svg";
-import {useEffect, useRef, useState} from "react";
-function VideoPlayer(){
-  // 播放状态
-  const [isPlaying, setIsPlaying] = useState(false);
-  // 播放器DOM
-  const videoRef = useRef();
-  //直接在组件中调用没有效果，因为这个时候DOM还没有渲染出来，需要在useEffect中调用，
-  // 也就是在组件渲染完成之后调用
-  // videoRef.current.play()
-  useEffect(()=>{
-    videoRef.current.play()
-    setIsPlaying(true)
-  },[])
-  // 播放
-  function  handlePlay(){
-    // 获取DOM
-    const video = videoRef.current;
-    // 播放
-    if (isPlaying) {
-      // 这个pause和play是video自带的方法
-      video.pause();
-    } else {
-      video.play();
-    }
-    // 更新播放状态
-    setIsPlaying(!isPlaying);
-  }
+import {forwardRef} from "react";
+function VideoPlayer({src},ref){
   return (
     <>
       {/*视频有声音的话，浏览器默认视频关闭状态*/}
-      <video ref={videoRef} src={Sea} width='320' muted controls></video>
-      <button onClick={handlePlay}>
-        {isPlaying? <PauseComp /> : <PlayComp />}
-      </button>
+      <video ref={ref} src={src} width='320' muted controls></video>
     </>
   )
 }
-export default VideoPlayer
+export default forwardRef(VideoPlayer)

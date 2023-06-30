f9f2d447b960fdc112e2b0d93e00b1520914265b
sleepyheadp <17863965581@163.com>
2023-06-30 15:24:56 +0800

    2023-06-30：路由传参的三种方式（query/search/state

--- src/router/routes.js
+++ src/router/routes.js
@@ -37,7 +37,7 @@ const routes = [
 		component: lazy(() => import("../views/B")),
 	},
 	{
-		path: "/c",
+		path: "/c/:id?/:name?",
 		name: "c",
 		component: lazy(() => import("../views/C")),
 	},

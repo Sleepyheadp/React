9f75a544a0338b3ae553b678b35f618a5d03f831
sleepyheadp <17863965581@163.com>
2023-11-14 16:09:01 +0800

    mobx@5：@observable是基于ES6的Proxy对数据进行劫持（get/set）

--- src/store/mbox5.js
+++ src/store/mbox5.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { observable, action, autorun } from "mobx";
 import { observer } from "mobx-react";
-import { AutoComplete } from "antd";
 /* 创建一个容器 */
 class Store {
 	// 公共状态:observable把数据变成可观察的
@@ -9,7 +8,6 @@ class Store {
 	// 公共方法:action把方法变成可观察的
 	@action changeCount() {
 		this.count++;
-		console.log("changeCount方法执行了", this.count);
 	}
 }
 let store = new Store();
@@ -27,7 +25,7 @@ class Mbox5 extends React.Component {
 } */
 autorun(() => {
 	// 首先在页面加载后会立即执行一次，然后每次数据「数据需要是可观察的」变化都会执行
-	console.log("autorun", store.count);
+	// console.log("autorun", store.count);
 });
 const Mbox5 = observer(() => {
 	return (
@@ -38,4 +36,24 @@ const Mbox5 = observer(() => {
 		</div>
 	);
 });
+// ES6的Proxy（示例代码）为了证明observable是基于Proxy实现的
+let obj = {
+	x: 10,
+	y: 20,
+};
+let proxyObj = new Proxy(obj, {
+	get(target, key) {
+		console.log("GETTER");
+		return target[key];
+	},
+	set(target, key, val) {
+		console.log("SETTER");
+		target[key] = val;
+		return key;
+	},
+});
+console.log(proxyObj); //返回的代理对象是被劫持的
+console.log(proxyObj.x); //获取某个成员值的时候，就会触发get函数
+proxyObj.x = 1000; //设置某个成员值的时候，就会触发set函数
+
 export default Mbox5;

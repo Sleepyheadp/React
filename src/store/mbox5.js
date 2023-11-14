27625d9db57dcfbca16b6ff959020ee7bd4f03b8
sleepyheadp <17863965581@163.com>
2023-11-14 17:31:49 +0800

    del other code

--- src/store/mbox5.js
+++ src/store/mbox5.js
@@ -1,37 +1,20 @@
 import React from "react";
 import { observable, action, autorun, observe } from "mobx";
 import { observer } from "mobx-react";
-/* 创建一个容器 */
 class Store {
 	// 公共状态:observable把数据变成可观察的
 	@observable count = 0;
-	// 对象形式
-	@observable objS = { a: 10, b: 20 };
-	// 公共方法:action把方法变成可观察的
 	@action changeCount() {
 		this.count++;
 	}
-	@action changeObj() {
-		this.objS.a++;
-	}
 }
 let store = new Store();
-/* @observer //在类组件上使用
-class Mbox5 extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>mobx5</h1>
-				<p>{store.count}</p>
-				<button onClick={() => store.changeCount()}>点击</button>
-			</div>
-		);
-	}
-} */
+
 autorun(() => {
 	// 首先在页面加载后会立即执行一次，然后每次数据「数据需要是可观察的」变化都会执行
-	// console.log("autorun", store.count);
+	console.log("autorun", store.count);
 });
+
 const Mbox5 = observer(() => {
 	return (
 		<div>
@@ -41,38 +24,4 @@ const Mbox5 = observer(() => {
 		</div>
 	);
 });
-// Tips: observable无法直接修饰原始值，需要使用observable.box
-/* let num = observable.box(10);
-console.log(num.get()); //获取值
-num.set(100); //设置值
-console.log(num.get()); */
-/* 探索observable的原理 */
-let obj2 = observable({
-	x: 10,
-	y: 20,
-});
-observe(obj2, (change) => {
-	console.log("change", change);
-});
-obj2.x = 1000;
-/* ES6的Proxy（示例代码）为了证明observable是基于Proxy实现的 */
-let obj = {
-	x: 10,
-	y: 20,
-};
-let proxyObj = new Proxy(obj, {
-	get(target, key) {
-		console.log("GETTER");
-		return target[key];
-	},
-	set(target, key, val) {
-		console.log("SETTER");
-		target[key] = val;
-		return key;
-	},
-});
-console.log(proxyObj); //返回的代理对象是被劫持的
-console.log(proxyObj.x); //获取某个成员值的时候，就会触发get函数
-proxyObj.x = 1000; //设置某个成员值的时候，就会触发set函数
-
 export default Mbox5;

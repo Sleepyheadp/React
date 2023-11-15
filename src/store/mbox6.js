a23324ebea3cffd2cb2902617393f2844e20f0a7
sleepyheadp <17863965581@163.com>
2023-11-15 15:19:33 +0800

    mobx@6:点击自增案例mobx@6的写法

---
+++ src/store/mbox6.js
@@ -0,0 +1,74 @@
+import {
+	makeAutoObservable,
+	autorun,
+	reaction,
+	runInAction,
+	computed,
+} from "mobx";
+import { observer } from "mobx-react";
+
+/* 模拟异步获取数据 */
+const getData = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(100);
+		}, 1000);
+	});
+};
+
+class Store {
+	count = 1;
+	num = 2;
+	obj = {
+		name: "张三",
+		age: 18,
+	};
+
+	constructor(root) {
+		this.root = root;
+		makeAutoObservable(this);
+	}
+
+	@computed get sum() {
+		console.log("sum方法执行了");
+		return this.count + this.num;
+	}
+	async changeCount() {
+		let res = 0;
+		try {
+			res = await getData();
+		} catch (_) {}
+		runInAction(() => {
+			this.count += res;
+		});
+	}
+}
+let store = new Store();
+
+autorun(() => {
+	console.log(
+		`autorun;${store.count}+${store.num}=${store.sum};${store.obj.age}`
+	);
+});
+
+reaction(
+	() => [store.sum, store.obj.age], // 监听的数据
+	() => {
+		console
+			.log
+			// `reaction;${store.count}+${store.num}=${store.sum};${store.obj.age}`
+			();
+	}
+);
+
+const Mbox6 = observer(() => {
+	return (
+		<div>
+			<h1>mobx6</h1>
+			<p>count:{store.count}</p>
+			<p>age:{store.obj.age}</p>
+			<button onClick={() => store.changeCount()}>点击</button>
+		</div>
+	);
+});
+export default Mbox6;
